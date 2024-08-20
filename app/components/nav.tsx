545b6ee209eb21fed2d30b084443e951f8d18f33
simran <simran@DESKTOP-8LLIKV0>
2024-08-20 15:58:33 -0400

    working on nav bar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,33 @@
-export default function Nav () {
+export default function Nav() {
     return (
-    <div>
-        NAV
-    </div>
-    )
+        <nav className="flex bg-gray-800 p-4 ">
+            <div className="text-lg font-bold text-white">
+                Logo
+            </div>
+            <div className="flex-1 flex justify-center bg-blue-200 mr-8">
+                <ul className="space-x-4">
+                    <li >
+                    <a href="#" className="text-white hover:underline">Events</a>
+                    </li>
+
+                    <li >
+                        
+                    <a href="#" className="text-white hover:underline">GitHub</a>
+                    </li>
+                    <li>
+                        
+                    <a href="#" className="text-white hover:underline">Meetup</a>
+                    </li>
+                    <li>
+                        
+                    <a href="#" className="text-white hover:underline">Discord</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
+
+// remember to add external link icon for accessibility
+// make it sticky 
+// get rid of the innate list formatting 
