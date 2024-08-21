10ebeb7024950a862a19ce9d58045e3ee066ac6a
simran <simran@DESKTOP-8LLIKV0>
2024-08-21 15:02:05 -0400

    nav bar is aligned. using fixed property. events link is missing but other links have been added.

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,33 +1,48 @@
-export default function Nav() {
-    return (
-        <nav className="flex bg-gray-800 p-4 ">
-            <div className="text-lg font-bold text-white">
-                Logo
-            </div>
-            <div className="flex-1 flex justify-center bg-blue-200 mr-8">
-                <ul className="space-x-4">
-                    <li >
-                    <a href="#" className="text-white hover:underline">Events</a>
-                    </li>
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex bg-gray-900 text-white py-4">
+      <a href="#" className="bg-green-400">
+        <img src="" alt="Flushing Tech Home" />
+      </a>
+      <div className="flex flex-1 justify-center bg-orange-400 mr-8 ">
+        <ul className="flex space-x-4">
+          <li>
+            <a href="#events" className="hover:text-gray-400">
+              Events
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://discord.gg/xGgFcZknDR"
+              className="hover:text-gray-400"
+            >
+              Discord
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.meetup.com/flushing-tech/"
+              className="hover:text-gray-400"
+            >
+              Meetup
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/flushingtech"
+              className="flex items-center hover:text-gray-400"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-                    <li >
-                        
-                    <a href="#" className="text-white hover:underline">GitHub</a>
-                    </li>
-                    <li>
-                        
-                    <a href="#" className="text-white hover:underline">Meetup</a>
-                    </li>
-                    <li>
-                        
-                    <a href="#" className="text-white hover:underline">Discord</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
-}
-
-// remember to add external link icon for accessibility
-// make it sticky 
-// get rid of the innate list formatting 
+export default Navbar;
+// check if "sticky"
+// make responsive for mobile
+// input the logo
+// need to get the events piece to jump to events section
