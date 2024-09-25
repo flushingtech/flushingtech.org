2eb527578ee65b874766c0c03e799752e634f8a6
Simran Kaur <simran00kk@gmail.com>
2024-09-25 12:11:46 -0400

    renamed folder

---
+++ app/components-deprecated/EventSection.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import eventData from "../../events.json"
+import { useState } from "react";
+import EventCard from "./EventCard";
+// Define the interface for the props
+
+
+const Events = () => {
+    const [currDescrip, setCurrDescrip]= useState("click event card to learn more")
+    return (
+        <section id="events">
+            <div className="event-cards">
+                {eventData.map((event, index) => (
+                    <EventCard 
+                        key={index}
+                        title={event.title}
+                        image_URL={event.image_URL}
+                        description={event.description}
+                        setCurrDescrip={setCurrDescrip} // Pass setCurrEvent to EventCard
+                    />
+                ))}
+            </div>
+            
+            <div>
+                {currDescrip}
+            </div>
+        </section>
+      
+    );
+  };
+  
+  export default Events;
