2a8312a1cf98efb39e944a23ad102fe1fb96312e
Ben Huang <benjaminwong1985@gmail.com>
2024-11-20 11:34:36 -0500

    feat: create a component to contain the social icons

--- app/components/header/SocialIcons.tsx
+++ app/components/header/SocialIcons.tsx
@@ -1,1 +1,23 @@
-export default function Main() {}
+import FontAwesomeIconBrand from "@/app/components/utils/FontAwesomeIconBrand";
+import Link from "next/link";
+import navItems from "@/app/components/header/navItems";
+export default function Main() {
+  return (
+    <div className="flex justify-end items-center">
+      {navItems.map((item) => {
+        if (item.faIcon != null)
+          return (
+            <div key={item.text}>
+              <Link
+                href={item.href}
+                target="_blank"
+                className="text-primary hover:text-site_orange transition-colors duration-300 text-2xl font-titillium mr-4"
+              >
+                <FontAwesomeIconBrand icon={item.faIcon} />
+              </Link>
+            </div>
+          );
+      })}
+    </div>
+  );
+}
