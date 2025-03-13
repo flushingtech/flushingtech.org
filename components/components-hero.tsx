08c3b0214fefd3f5344a3cebc3e7ea209d498d43
benhuangbmj <benjaminwong1985@gmail.com>
2025-03-13 12:02:37 -0400

    chore: remove the unwanted border

--- components/components-hero.tsx
+++ components/components-hero.tsx
@@ -16,7 +16,7 @@ export default function HeroComponent() {
       }}
     >
       <div className="hidden md:block w-[30%] max-w-[600px] h-full bg-gradient-to-b from-black/85 via-black/50 to-black/10 absolute right-0 top-0 py-4 px-2 text-peach">
-        <div className="w-fit mx-auto text-3xl lg:text-4xl font-bold font-site_header border border-white text-center">
+        <div className="w-fit mx-auto text-3xl lg:text-4xl font-bold font-site_header text-center">
           Hackathon <span className="text-site_orange">Leaderboard</span>
         </div>
         <LeaderboardContent
