dce297a847a9215294599b7eb89652d90c7ab0a7
benhuangbmj <benjaminwong1985@gmail.com>
2025-03-14 17:25:56 -0400

    chore: deploy the leaderboard container

--- components/components-hero.tsx
+++ components/components-hero.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import LeaderboardContent from "@/app/components/LeaderboardContent";
-
+import LeaderboardContainer from "@/app/components/leaderboard/LeaderboardContainer";
 export default function HeroComponent() {
   return (
     <section
@@ -15,16 +14,7 @@ export default function HeroComponent() {
         height: "calc(100vh - 4em)",
       }}
     >
-      <div className="hidden md:block w-[30%] max-w-[600px] h-full bg-gradient-to-b from-black/85 via-black/50 to-black/10 absolute right-0 top-0 py-4 px-2 text-peach">
-        <div className="w-fit mx-auto text-3xl lg:text-4xl font-bold font-site_header text-center">
-          Hackathon <span className="text-site_orange">Leaderboard</span>
-        </div>
-        <LeaderboardContent
-          title="Total Wins"
-          gridTemplate="grid-cols-[8em_1fr_1em]"
-          src="leaderboardmostwins"
-        />
-      </div>
+      <LeaderboardContainer />
       <div className="container mx-auto px-4 lg:py-[10%] h-full flex flex-col justify-center item-center">
         <div className="md:w-[70%] py-[5%]">
           <h1 className="text-4xl lg:text-5xl font-bold mb-4 font-site_header">
