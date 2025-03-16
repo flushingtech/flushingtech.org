384bd33039cc95e947b96b46822a693b007d5b87
benhuangbmj <benjaminwong1985@gmail.com>
2025-03-16 13:48:28 -0400

    feat: build the leaderboard selection tab

--- app/components/leaderboard/Leaderboard.tsx
+++ app/components/leaderboard/Leaderboard.tsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import LeaderboardContent from "./LeaderboardContent";
 
 import { ContentArray } from "./LeaderboardContent";
 type MostWins = { contributor: string; total_wins: string };
+
+const tabsTriggerCN =
+  "font-site_button lg:text-base border border-peach border-2 p-2 data-[state=active]:bg-site_navy data-[state=active]:text-site_orange data-[state=active]:border-site_orange";
 
 export default function Leaderboard({ hidden = false }: { hidden: boolean }) {
   const [mostWins, setMostWins] = useState<MostWins[] | null>();
   const [contentMostWins, setContentMostWins] = useState<
     ContentArray[] | null
   >();
+  const [selected, setSelected] = useState<string>("Total Wins");
   useEffect(() => {
     fetch(
       "https://votte-backend.flushingtech.org/api/leaderboard/leaderboardmostwins"
@@ -16,7 +21,46 @@ export default function Leaderboard({ hidden = false }: { hidden: boolean }) {
       .then((res) => res.json())
       .then((data) => setMostWins(data.leaderboard));
   }, []);
+
   useEffect(() => {
+    processContentMostWins();
+  }, [mostWins]);
+
+  if (hidden) return null;
+
+  return (
+    <>
+      <div className="my-8 text-right ">
+        <Tabs
+          defaultValue="Total Wins"
+          className="w-fit inline-block"
+          onValueChange={handleTabValueChange}
+        >
+          <TabsList className="bg-site_navy text-white font-site_button h-auto">
+            <TabsTrigger className={tabsTriggerCN} value="Total Wins">
+              Total Wins
+            </TabsTrigger>
+            <TabsTrigger className={tabsTriggerCN} value="Recent Winners">
+              Recent Winners
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+
+      {contentMostWins && selected === "Total Wins" && (
+        <LeaderboardContent
+          title="Total Wins"
+          gridTemplate="grid-cols-[8em_1fr_1em]"
+          content={contentMostWins}
+        />
+      )}
+    </>
+  );
+
+  function handleTabValueChange(value: string) {
+    setSelected(value);
+  }
+  function processContentMostWins() {
     if (mostWins) {
       const content: ContentArray[] = mostWins.map(
         (e: MostWins, _: number, arr: any[]) => [
@@ -30,15 +74,5 @@ export default function Leaderboard({ hidden = false }: { hidden: boolean }) {
       );
       setContentMostWins(content);
     }
-  }, [mostWins]);
-  return (
-    !hidden &&
-    contentMostWins && (
-      <LeaderboardContent
-        title="Total Wins"
-        gridTemplate="grid-cols-[8em_1fr_1em]"
-        content={contentMostWins}
-      />
-    )
-  );
+  }
 }
