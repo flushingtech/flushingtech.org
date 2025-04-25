7bbc98b10010c436e4660119b870104f823730db
Marc Anthony Tumminello <marcanthonytumminello@mail.adelphi.edu>
2025-04-24 20:28:19 -0400

    fixed columns grid layout

--- app/components/benefits/Benefits.tsx
+++ app/components/benefits/Benefits.tsx
@@ -6,12 +6,12 @@ import { BenefitsType } from './benefits-data';
 const Benefits = () => {
   return (
     <>
-      <div className='fav-res-container flex flex-col gap-16 container px-16 mx-auto min-h-screen py-8'>
+      <div className='fav-res-container flex flex-col gap-16 container sm:px-16 px-4 items-center justify-center min-h-screen py-8'>
         <div className='favorite-heading flex flex-col gap-2'>
           <h2 className='text-4xl font-semibold text-center font-site_header'>Benefits of Joining Flushing Tech</h2>
           <p className='text-muted-foreground text-xl text-center'>There are many!</p>
         </div>
-        <div className='cards-container grid grid-cols-2 md:grid-cols-5 gap-4 place-content-center'>
+        <div className='cards-container grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-8 place-content-center'>
           {BENEFITS_DATA.map(({ title, description }:BenefitsType ) => (
             <BenefitsCard title={title} description={description} />
           ))}
