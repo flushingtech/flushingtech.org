7e75b55be74e29cd178a8fe8ba58b549af1bd9db
Denakpo Agba <67210181+agbagerard@users.noreply.github.com>
2025-04-27 21:50:37 -0400

    added votte link in components-projects.tsx (#160)

--- components/components-projects.tsx
+++ components/components-projects.tsx
@@ -1,125 +1,142 @@
-"use client";
+'use client';
 
-import { useState, useCallback, useEffect } from "react";
-import useEmblaCarousel from "embla-carousel-react";
-import Autoplay from "embla-carousel-autoplay";
-import { Button } from "@/components/ui/button";
+import { useState, useCallback, useEffect } from 'react';
+import Image from 'next/image';
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 type Project = {
-  title: string;
-  description: string;
-  image: string;
-  link: string;
+	title: string;
+	description: string;
+	image: string;
+	link: string;
+};
+
+type External = {
+	text: string;
+	link: string;
+};
+
+const VotteInfo: External = {
+	text: '👉 Vote on the next hackathon ideas! 👈',
+	link: process.env.NEXT_PUBLIC_VOTTE_URL as string,
 };
 
 const projects: Project[] = [
-  {
-    title: "SpaceX UPS",
-    description:
-      "Save API data to local file, in this case SpaceX launch data to an output.json. Built using Java with Maven.",
-    image: "/spacex-ups.jpg",
-    link: "https://github.com/AaronNewTech/space-scanner",
-  },
-  {
-    title: "Wearable",
-    description:
-      "Explore building wearables by learning Arduino basics. This project got a simple blinking program to run on the Arduino.",
-    image: "/wearables.jpg",
-    link: "",
-  },
-  {
-    title: "Smart Homes",
-    description:
-      "Explore Department of Buildings data to help build potential smart home solutions.",
-    image: "/smarthome-dob.jpg",
-    link: "",
-  },
-  {
-    title: "Votte",
-    description:
-      "Deployed the Votte app to Vercel, and started connecting it to a database powered by Neon.tech",
-    image: "/votte.jpg",
-    link: "https://github.com/flushingtech/Votte_Backend",
-  },
+	{
+		title: 'SpaceX UPS',
+		description:
+			'Save API data to local file, in this case SpaceX launch data to an output.json. Built using Java with Maven.',
+		image: '/spacex-ups.jpg',
+		link: 'https://github.com/AaronNewTech/space-scanner',
+	},
+	{
+		title: 'Wearable',
+		description:
+			'Explore building wearables by learning Arduino basics. This project got a simple blinking program to run on the Arduino.',
+		image: '/wearables.jpg',
+		link: '',
+	},
+	{
+		title: 'Smart Homes',
+		description:
+			'Explore Department of Buildings data to help build potential smart home solutions.',
+		image: '/smarthome-dob.jpg',
+		link: '',
+	},
+	{
+		title: 'Votte',
+		description:
+			'Deployed the Votte app to Vercel, and started connecting it to a database powered by Neon.tech',
+		image: '/votte.jpg',
+		link: 'https://github.com/flushingtech/Votte_Backend',
+	},
 ];
 
 export default function ProjectsCarouselComponent() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
+	const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+	const [selectedIndex, setSelectedIndex] = useState(0);
 
-  const scrollTo = useCallback(
-    (index: number) => {
-      if (emblaApi) emblaApi.scrollTo(index);
-    },
-    [emblaApi]
-  );
+	const scrollTo = useCallback(
+		(index: number) => {
+			if (emblaApi) emblaApi.scrollTo(index);
+		},
+		[emblaApi]
+	);
 
-  const onSelect = useCallback(() => {
-    if (!emblaApi) return;
-    setSelectedIndex(emblaApi.selectedScrollSnap());
-  }, [emblaApi]);
+	const onSelect = useCallback(() => {
+		if (!emblaApi) return;
+		setSelectedIndex(emblaApi.selectedScrollSnap());
+	}, [emblaApi]);
 
-  useEffect(() => {
-    if (!emblaApi) return;
-    onSelect();
-    emblaApi.on("select", onSelect);
-    return () => {
-      emblaApi.off("select", onSelect);
-    };
-  }, [emblaApi, onSelect]);
+	useEffect(() => {
+		if (!emblaApi) return;
+		onSelect();
+		emblaApi.on('select', onSelect);
+		return () => {
+			emblaApi.off('select', onSelect);
+		};
+	}, [emblaApi, onSelect]);
 
-  return (
-    <section className="bg-peach overflow-x-hidden">
-      <div className="w-screen flex flex-col item-center pt-10 pb-4 md:pt-28 md:pb-36">
-        <h2 className="block text-4xl font-site_header text-center font-bold">
-          {"//"}Check Out Our <span className="text-site_red">Projects</span>
-        </h2>
-        <div ref={emblaRef}>
-          <div className="flex mt-10 md:mt-20">
-            {projects.map((project, index) => (
-              <div key={index} className="flex-[0_0_100%]">
-                <div className="container w-[80%] flex flex-col md:flex-row gap-8 items-center mx-auto">
-                  <div className="md:w-1/2 relative">
-                    <a {...(project.link !== "" ? { href: project.link } : {})}>
-                      <img
-                        src={project.image}
-                        alt={`${project.title} Image`}
-                        className="border-4 border-site_red w-full"
-                      />
-                    </a>
-                    <div className="absolute bottom-8 left-0 right-0 flex justify-center">
-                      <div className="flex space-x-8">
-                        {projects.map((_, dotIndex) => (
-                          <Button
-                            key={dotIndex}
-                            variant="ghost"
-                            size="sm"
-                            className={`w-5 h-5 p-0 rounded-full ${
-                              dotIndex === selectedIndex
-                                ? "bg-site_red"
-                                : "bg-site_orange"
-                            }`}
-                            onClick={() => scrollTo(dotIndex)}
-                            aria-label={`Go to project ${dotIndex + 1}`}
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="md:w-1/2">
-                    <h3 className="text-3xl font-bold font-site_header m-auto mb-12 text-gray-900 text-center md:w-1/2 md:text-5xl">
-                      {project.title}
-                    </h3>
-                    <p className="text-lg m-auto mb-6 text-gray-700 font-site_1st_paragraph text-center md:w-3/4 md:text-3xl">
-                      {project.description}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className='bg-peach overflow-x-hidden'>
+			<div className='w-screen flex flex-col item-center pt-10 pb-4 md:pt-28 md:pb-36'>
+				<h2 className='block text-4xl font-site_header text-center font-bold'>
+					{'//'}Check Out Our <span className='text-site_red'>Projects</span>
+				</h2>
+				<div className='mt-4 text-4xl text-site_orange text-center hover:bg-sky-700 text-site_header'>
+					<Link href={VotteInfo.link}>{VotteInfo.text}</Link>
+				</div>
+				<div ref={emblaRef}>
+					<div className='flex mt-10 md:mt-20'>
+						{projects.map((project, index) => (
+							<div key={index} className='flex-[0_0_100%]'>
+								<div className='container w-[80%] flex flex-col md:flex-row gap-8 items-center mx-auto'>
+									<div className='md:w-1/2 relative'>
+										<a {...(project.link !== '' ? { href: project.link } : {})}>
+											<Image
+												src={project.image}
+												alt={`${project.title} Image`}
+												width={1000}
+												height={1000}
+												className='border-4 border-site_red w-full'
+											/>
+										</a>
+										<div className='absolute bottom-8 left-0 right-0 flex justify-center'>
+											<div className='flex space-x-8'>
+												{projects.map((_, dotIndex) => (
+													<Button
+														key={dotIndex}
+														variant='ghost'
+														size='sm'
+														className={`w-5 h-5 p-0 rounded-full ${
+															dotIndex === selectedIndex
+																? 'bg-site_red'
+																: 'bg-site_orange'
+														}`}
+														onClick={() => scrollTo(dotIndex)}
+														aria-label={`Go to project ${dotIndex + 1}`}
+													/>
+												))}
+											</div>
+										</div>
+									</div>
+									<div className='md:w-1/2'>
+										<h3 className='text-3xl font-bold font-site_header m-auto mb-12 text-gray-900 text-center md:w-1/2 md:text-5xl'>
+											{project.title}
+										</h3>
+										<p className='text-lg m-auto mb-6 text-gray-700 font-site_1st_paragraph text-center md:w-3/4 md:text-3xl'>
+											{project.description}
+										</p>
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
