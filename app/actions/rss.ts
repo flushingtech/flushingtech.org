eae02c902b732cf17b4f4c4e5cd5dc3e3b97eef7
benhuangbmj <benjaminwong1985@gmail.com>
2025-05-05 00:25:23 -0400

    fix: expand eslint config and fix linting errors

--- app/actions/rss.ts
+++ app/actions/rss.ts
@@ -1,15 +1,16 @@
-"use server";
+'use server';
 
 export async function fetchRSS(url: string): Promise<string> {
-	try {
-		const res = await fetch(url);
-		if (res.ok) {
-			const output = await res.text();
-			return output;
-		} else {
-			throw Error(res.status.toString());
-		}
-	} catch (err) {
-		throw err;
-	}
+  try {
+    const res = await fetch(url);
+    if (res.ok) {
+      const output = await res.text();
+      return output;
+    } else {
+      throw Error(res.status.toString());
+    }
+  } catch (err) {
+    console.error('Error fetching RSS feed:', err);
+    throw err;
+  }
 }
