929a110a300854dd58f2e5f3679a91bab0e16266
tetron <skim6502@gmail.com>
2025-05-14 15:48:47 -0400

    added a module import since theres two functions located in project components. One for Carousel, One for Gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import HeroComponent from "@/components/components-hero"
-import ProjectsComponent from "@/components/components-projects"
+import {GridLayout, ProjectsCarouselComponent} from "@/components/components-projects"
 import ConnectWithUsComponent from "@/components/components-connect-with-us"
 
 export default function Homepage() {
   return (
     <div className="min-h-screen bg-[#FBE8D8]">
       <HeroComponent />
-      <ProjectsComponent />
+      <GridLayout />
       <ConnectWithUsComponent />
     </div>
   )
